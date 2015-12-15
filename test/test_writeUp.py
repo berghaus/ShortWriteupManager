efc3d67f21d130a715912e361bbcdca672ed4612
Frank Berghaus <frank.berghaus@cern.ch>
2015-12-15 13:43:56 +0100

    Set up Travis-CI testing
    Using the default example for continuous integration with Travis. Right
    now there should be extra required modules. Though re, itertools, and
    datetime may come around to bite me.

--- test/test_writeUp.py
+++ test/test_writeUp.py
@@ -10,26 +10,26 @@ from datetime import date
 class TDDWriteUp(unittest.TestCase):
 
     def test_write_up_stores_filename(self):
-        write_up = WriteUp("test.tex")
+        write_up = WriteUp("test/test.tex")
         result = write_up._fileName
-        self.assertEqual("test.tex", result)
+        self.assertEqual("test/test.tex", result)
 
     def test_write_up_parses_title(self):
-        write_up = WriteUp("test.tex")
+        write_up = WriteUp("test/test.tex")
         self.assertEqual('Manipulation of Triangular and Symmetric Matrices',write_up.title)
 
     def test_write_up_parses_author(self):
-        write_up = WriteUp("test.tex")
+        write_up = WriteUp("test/test.tex")
         result = write_up.author
         self.assertEqual("W. Hart", result)
 
     def test_write_up_parses_ID(self):
-        write_up = WriteUp("test.tex")
+        write_up = WriteUp("test/test.tex")
         result = write_up.ID
         self.assertEqual("F112", result)
 
     def test_write_up_parses_keywords(self):
-        write_up = WriteUp("test.tex")
+        write_up = WriteUp("test/test.tex")
         self.assertIn("operation",write_up.keywords)
         self.assertIn("symmetric",write_up.keywords)
         self.assertIn("triangular",write_up.keywords)
@@ -43,7 +43,7 @@ class TDDWriteUp(unittest.TestCase):
         self.assertIn("residue",write_up.keywords)
 
     def test_write_up_parses_routines(self):
-        write_up = WriteUp("test.tex")
+        write_up = WriteUp("test/test.tex")
         self.assertIn("trchul",write_up.routines)
         self.assertIn("trchlu",write_up.routines)
         self.assertIn("trsmul",write_up.routines)
@@ -51,23 +51,23 @@ class TDDWriteUp(unittest.TestCase):
         self.assertIn("trinv",write_up.routines)
 
     def test_write_up_parses_library(self):
-        write_up = WriteUp("test.tex")
+        write_up = WriteUp("test/test.tex")
         self.assertEqual("kernlib",write_up.library)
 
     def test_write_up_parses_language(self):
-        write_up = WriteUp("test.tex")
+        write_up = WriteUp("test/test.tex")
         self.assertEqual("fortran",write_up.language)
 
     def test_write_up_parses_submitter(self):
-        write_up = WriteUp("test.tex")
+        write_up = WriteUp("test/test.tex")
         self.assertEqual("Waggleton P. Tallylicker",write_up.submitter)
 
     def test_write_up_parses_submitted(self):
-        write_up = WriteUp("test.tex")
+        write_up = WriteUp("test/test.tex")
         self.assertEqual(date(1975,1,1),write_up.submitted)
 
     def test_write_up_parses_revised(self):
-        write_up = WriteUp("test.tex")
+        write_up = WriteUp("test/test.tex")
         self.assertEqual(date(1986,12,12),write_up.revised)
 
 
