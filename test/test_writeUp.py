6109e2aaee00f641594df5b940b27f9907dc47e0
Frank Berghaus <frank.berghaus@cern.ch>
2015-12-15 13:38:12 +0100

    Insert block to ensure python 3 compatibility
    Decided to build me code to be compatible with python3. This may be
    necessary since Antonio's code seemed to rely on python3.

--- test/test_writeUp.py
+++ test/test_writeUp.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import unittest
 from writeup.writeup import WriteUp
 from datetime import date
