4b11f7c8b82d9fbee8ef524c758bb2a6b9f93302
Frank Berghaus <frank.berghaus@cern.ch>
2015-12-15 13:28:41 +0100

    Add WriteUp class that will contain each write-up
    Implemented the writeup class with a constructor that parses the
    standard organization of the CERNLIN short write-ups. The class now
    knows what title, author, ID, keywords, routines, library, programming
    language, submitter, and sumbission and revision dates to associate with
    the writeup.
    
    I am following test driven design. The test directory contains the test
    implementations and the data needed for the tests.

---
+++ test/test_writeUp.py
@@ -0,0 +1,70 @@
+import unittest
+from writeup.writeup import WriteUp
+from datetime import date
+
+class TDDWriteUp(unittest.TestCase):
+
+    def test_write_up_stores_filename(self):
+        write_up = WriteUp("test.tex")
+        result = write_up._fileName
+        self.assertEqual("test.tex", result)
+
+    def test_write_up_parses_title(self):
+        write_up = WriteUp("test.tex")
+        self.assertEqual('Manipulation of Triangular and Symmetric Matrices',write_up.title)
+
+    def test_write_up_parses_author(self):
+        write_up = WriteUp("test.tex")
+        result = write_up.author
+        self.assertEqual("W. Hart", result)
+
+    def test_write_up_parses_ID(self):
+        write_up = WriteUp("test.tex")
+        result = write_up.ID
+        self.assertEqual("F112", result)
+
+    def test_write_up_parses_keywords(self):
+        write_up = WriteUp("test.tex")
+        self.assertIn("operation",write_up.keywords)
+        self.assertIn("symmetric",write_up.keywords)
+        self.assertIn("triangular",write_up.keywords)
+        self.assertIn("matrix",write_up.keywords)
+        self.assertIn("number",write_up.keywords)
+        self.assertIn("zero",write_up.keywords)
+        self.assertIn("complex",write_up.keywords)
+        self.assertIn("analytic",write_up.keywords)
+        self.assertIn("function",write_up.keywords)
+        self.assertIn("logarithmic",write_up.keywords)
+        self.assertIn("residue",write_up.keywords)
+
+    def test_write_up_parses_routines(self):
+        write_up = WriteUp("test.tex")
+        self.assertIn("trchul",write_up.routines)
+        self.assertIn("trchlu",write_up.routines)
+        self.assertIn("trsmul",write_up.routines)
+        self.assertIn("trsmlu",write_up.routines)
+        self.assertIn("trinv",write_up.routines)
+
+    def test_write_up_parses_library(self):
+        write_up = WriteUp("test.tex")
+        self.assertEqual("kernlib",write_up.library)
+
+    def test_write_up_parses_language(self):
+        write_up = WriteUp("test.tex")
+        self.assertEqual("fortran",write_up.language)
+
+    def test_write_up_parses_submitter(self):
+        write_up = WriteUp("test.tex")
+        self.assertEqual("Waggleton P. Tallylicker",write_up.submitter)
+
+    def test_write_up_parses_submitted(self):
+        write_up = WriteUp("test.tex")
+        self.assertEqual(date(1975,1,1),write_up.submitted)
+
+    def test_write_up_parses_revised(self):
+        write_up = WriteUp("test.tex")
+        self.assertEqual(date(1986,12,12),write_up.revised)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ writeup/writeup.py
@@ -0,0 +1,52 @@
+import re
+from itertools import chain
+from datetime import date
+from datetime import datetime
+
+class WriteUp:
+
+    author = ''
+    ID = ''
+
+    def __init__(self, fileName):
+        self._fileName = fileName
+        with open(self._fileName, 'r') as file:
+            content = file.read()
+
+            # parse Author field from tex
+            self.title = re.search(r'.*\\Cernhead{(.*?)}', content).group(1).strip()
+
+            # parse Author field from tex
+            self.author = re.search(r'.*\\Author{(.*?)}', content).group(1).strip()
+
+            # parse submitter from tex
+            self.submitter = re.search(r'.*\\Submitter{(.*?)}', content).group(1).strip()
+
+            # parse Routine ID from tex
+            self.ID = re.search(r'.*\\Routid{(.*?)}', content).group(1).strip()
+
+            # parse keyword fields from tex
+            self.keywords = re.findall(r'\\Keywords{(.*?)}', content)
+            self.keywords = list(map(lambda s: s.split(), self.keywords))
+            self.keywords = list(chain.from_iterable(self.keywords))
+            self.keywords = list(map(lambda s: s.lower(), self.keywords))
+
+            # parse keyword fields from tex
+            self.routines = re.findall(r'\\Rdef{(.*?)}', content)
+            self.routines = list(map(lambda s: s.split(), self.routines))
+            self.routines = list(chain.from_iterable(self.routines))
+            self.routines = list(map(lambda s: s.lower(), self.routines))
+
+            # parse programming language from tex
+            self.language = re.search(r'.*\\Language{(.*?)}', content).group(1).lower().strip()
+
+            # parse library from tex
+            self.library = re.search(r'.*\\Library{(.*?)}', content).group(1).lower().strip()
+
+            # parse submission date from tex
+            bdt = datetime.strptime(re.search(r'.*\\Submitted{(.*?)}', content).group(1),"%d.%m.%Y")
+            self.submitted = date(bdt.year,bdt.month,bdt.day)
+
+            # parse revision date from tex
+            bdt = datetime.strptime(re.search(r'.*\\Revised{(.*?)}', content).group(1),"%d.%m.%Y")
+            self.revised = date(bdt.year,bdt.month,bdt.day)
